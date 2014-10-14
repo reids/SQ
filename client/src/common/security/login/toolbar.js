75fae2031b756344c4ad85fb1849b97c3b479dd7
Reid Sommerville <reid.sommerville@gmail.com>
2014-10-14 23:00:31 +0200

    Getting user creation working

---
+++ client/src/common/security/login/toolbar.js
@@ -0,0 +1,27 @@
+angular.module('security.login.toolbar', [])
+
+// The loginToolbar directive is a reusable widget that can show login or logout buttons
+// and information the current authenticated user
+.directive('loginToolbar', ['security', '$location', function(security, $location) {
+  var directive = {
+    templateUrl: 'security/login/toolbar.tpl.html',
+    restrict: 'E',
+    replace: true,
+    scope: true,
+    link: function($scope, $element, $attrs, $location) {
+      $scope.isAuthenticated = security.isAuthenticated;
+      $scope.login = security.showLogin;
+      $scope.logout = security.logout;
+      $scope.$watch(function() {
+        return security.currentUser;
+      }, function(currentUser) {
+        $scope.currentUser = currentUser;
+      });
+      $scope.createUser =  function (){
+    	  // FIXME, what am I doing wrong with scope to use $location
+		  this.location.path('/admin/users/new');
+		};
+    }
+  };
+  return directive;
+}]);
