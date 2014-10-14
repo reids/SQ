58798ca54a97e7027d7af86973d9a814fb5d398e
Reid Sommerville <reid.sommerville@gmail.com>
2014-10-14 10:39:34 +0200

    Timezone display with ticking clocks

---
+++ client/src/app/timezones/timezones.js
@@ -0,0 +1,48 @@
+angular.module('timezones', ['resources.timezones', 'security.authorization'])
+
+.config(['$routeProvider', 'securityAuthorizationProvider', function ($routeProvider, securityAuthorizationProvider) {
+	  $routeProvider.when('/timezones', {
+	    templateUrl:'timezones/timezones-list.tpl.html',
+	    controller:'TimezonesViewCtrl',
+	    resolve:{
+	      timezones:['Timezones', function (Timezones) {
+	        return Timezones.all();
+	      }],
+	      authenticatedUser: securityAuthorizationProvider.requireAuthenticatedUser
+	    }
+	  });
+	}])
+
+.controller('TimezonesViewCtrl', ['$scope', '$timeout', '$location', 'timezones', 'security', function ($scope, $timeout, $location, timezones, security) {
+  $scope.timezones = timezones;
+
+  $scope.currtimes = [];
+  
+  $scope.currenttime = moment.tz('').format('HH:mm:ss');
+  angular.forEach($scope.timezones, function(timezone) {
+	    $scope.currtimes[timezone.$id()] = moment.tz(timezone.name).format('HH:mm:ss');
+	  });
+  
+  var tick = function() {
+    $scope.currenttime = moment.tz('').format('HH:mm:ss');
+    angular.forEach($scope.timezones, function(timezone) {
+	    $scope.currtimes[timezone.$id()] = moment.tz(timezone.name).format('HH:mm:ss');
+  	  });
+	$timeout(tick, 1000);
+  }
+	
+  tick();
+
+  $scope.viewTimezone = function (timezone) {
+    $location.path('/timezones/'+timezone.$id());
+  };
+
+  $scope.edit = function (timezone) {
+    $location.path('/timezones/'+timezone.$id()+'/edit');
+  };
+
+  $scope.remove = function (timezone) {
+    $location.path('/timezones/'+timezone.$id()+'/remove');
+  };
+
+}]);
