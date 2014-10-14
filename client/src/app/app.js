d570a13abd84ffbdc4451460caa6e361a27335d0
Reid Sommerville <reid.sommerville@gmail.com>
2014-10-14 22:59:59 +0200

    Multiple logon and dashboard changes

---
+++ client/src/app/app.js
@@ -0,0 +1,80 @@
+angular.module('app', [
+  'ngRoute',
+  'timezones',
+  'dashboard',
+  'admin',
+  'services.breadcrumbs',
+  'services.i18nNotifications',
+  'services.httpRequestTracker',
+  'security',
+  'directives.crud',
+  'templates.app',
+  'templates.common']);
+
+angular.module('app').constant('MONGOLAB_CONFIG', {
+  baseUrl: '/databases/',
+  dbName: 'mongogb1'
+});
+
+//TODO: move those messages to a separate module
+angular.module('app').constant('I18N.MESSAGES', {
+  'errors.route.changeError':'Route change error',
+  'crud.user.save.success':"A user with id '{{id}}' was saved successfully.",
+  'crud.user.save.error':"Something went wrong when saving a user...",
+  'crud.timezone.save.success':"A timezone with id '{{id}}' was saved successfully.",
+  'crud.timezone.remove.success':"A timezone with id '{{id}}' was removed successfully.",
+  'crud.timezone.save.error':"Something went wrong when saving a timezone...",
+  'login.reason.notAuthorized':"You do not have the necessary access permissions.  Do you want to login as someone else?",
+  'login.reason.notAuthenticated':"You must be logged in to access this part of the application.",
+  'login.error.invalidCredentials': "Login failed.  Please check your credentials and try again.",
+  'login.error.serverError': "There was a problem with authenticating: {{exception}}."
+});
+
+angular.module('app').config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
+  $routeProvider.otherwise({redirectTo:'/dashboard'});
+}]);
+
+angular.module('app').run(['security', function(security) {
+  // Get the current user when the application starts
+  // (in case they are still logged in from a previous session)
+  security.requestCurrentUser();
+}]);
+
+angular.module('app').controller('AppCtrl', ['$scope', 'i18nNotifications', 'localizedMessages', function($scope, i18nNotifications, localizedMessages) {
+
+  $scope.notifications = i18nNotifications;
+
+  $scope.removeNotification = function (notification) {
+    i18nNotifications.remove(notification);
+  };
+
+  $scope.$on('$routeChangeError', function(event, current, previous, rejection){
+    i18nNotifications.pushForCurrentRoute('errors.route.changeError', 'error', {}, {rejection: rejection});
+  });
+}]);
+
+angular.module('app').controller('HeaderCtrl', ['$scope', '$location', '$route', 'security', 'breadcrumbs', 'notifications', 'httpRequestTracker',
+  function ($scope, $location, $route, security, breadcrumbs, notifications, httpRequestTracker) {
+  $scope.location = $location;
+  $scope.breadcrumbs = breadcrumbs;
+
+  $scope.isAuthenticated = security.isAuthenticated;
+  $scope.isAdmin = security.isAdmin;
+
+  $scope.home = function () {
+    if (security.isAuthenticated()) {
+      $location.path('/timezones');
+    } else {
+      $location.path('/dashboard');
+    }
+  };
+
+  $scope.isNavbarActive = function (navBarPath) {
+    return navBarPath === breadcrumbs.getFirst().name;
+  };
+
+  $scope.hasPendingRequests = function () {
+    return httpRequestTracker.hasPendingRequests();
+  };
+}]);
