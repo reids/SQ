18919f4e1ef235a1cb08c77954309206d16b8f36
Reid Sommerville <reid.sommerville@gmail.com>
2014-10-21 22:04:26 +0200

    Message cleanup

--- client/src/app/admin/users/admin-users-edit.js
+++ client/src/app/admin/users/admin-users-edit.js
@@ -1,23 +1,21 @@
 angular.module('admin-users-edit',[
   'services.crud',
   'services.i18nNotifications',
-  'admin-users-edit-uniqueEmail',
   'admin-users-edit-validateEquals'
 ])
 
 .controller('UsersEditCtrl', ['$scope', '$location', 'i18nNotifications', 'user', function ($scope, $location, i18nNotifications, user) {
 
-  user.admin=true;
   $scope.user = user;
   $scope.password = user.password;
 
   $scope.onSave = function (user) {
-    i18nNotifications.pushForNextRoute('crud.user.save.success', 'success', {id : user.$id()});
+    i18nNotifications.pushForNextRoute('crud.user.save.success', 'success', {email : user.email});
     $location.path('/dashboard');
   };
 
-  $scope.onError = function() {
-    i18nNotifications.pushForCurrentRoute('crud.user.save.error', 'error');
+  $scope.onError = function(statusText) {
+    i18nNotifications.pushForCurrentRoute('crud.user.save.error', 'error', {statusText : statusText });
   };
 
   $scope.emailToLower = function() {
