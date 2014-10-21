0d7b34697ccca302eaccf76fd0a522c7e3b781d8
Reid Sommerville <reid.sommerville@gmail.com>
2014-10-21 22:12:14 +0200

    Fixed slowdown and umlaut issue, was all down to bad content length

--- server/lib/mongo-proxy.js
+++ server/lib/mongo-proxy.js
@@ -9,8 +9,8 @@ module.exports = function(basePath, apiKey, dbName) {
   // Map the request url to the mongolab url
   // @Returns a parsed Url object
   var mapUrl = module.exports.mapUrl = function(reqUrlString, reqQuery) {
-    var reqUrl = url.parse(reqUrlString, true);
-    var newUrl = {
+  var reqUrl = url.parse(reqUrlString, true);
+  var newUrl = {
       hostname: basePath.hostname,
       protocol: basePath.protocol
     };
@@ -27,19 +27,21 @@ module.exports = function(basePath, apiKey, dbName) {
 
   // Map the incoming request to a request to the DB
   var mapRequest = module.exports.mapRequest = function(req) {
-	var dbPath = '/databases/' + dbName + '/collections/' + req.params.collection + (req.params[0] ? req.params[0] : '');   
+	  var dbPath = '/databases/' + dbName + '/collections/' + req.params.collection + (req.params[0] ? req.params[0] : '');   
 	  var newReq = mapUrl(dbPath, req.query);
-//    var newReq = mapUrl(req.path, req.query);
-    newReq.method = req.method;
-    newReq.headers = req.headers || {};
-    // We need to fix up the hostname
-    newReq.headers.host = newReq.hostname;
-    return newReq;
+	  newReq.method = req.method;
+	  newReq.headers = req.headers || {};
+	  // Don't specify content length, let the server work it out, we've seen problems with umlauts.
+	  delete newReq.headers['content-length'];
+	  // We need to fix up the hostname
+	  newReq.headers.host = newReq.hostname;
+	  return newReq;
   };
 
   var proxy = function(req, res, next) {
     try {
       var options = mapRequest(req);
+      body=JSON.stringify(req.body);
       // Create the request to the db
       var dbReq = https.request(options, function(dbRes) {
         var data = "";
@@ -58,8 +60,9 @@ module.exports = function(basePath, apiKey, dbName) {
           res.end();
         });
       });
-      // Send any data the is passed from the original request
-      dbReq.end(JSON.stringify(req.body));
+      // Send any data the is passed from the original request, better way to detect an empty body?
+      dbReq.write(JSON.stringify(req.body))
+      dbReq.end();
     } catch (error) {
       console.log('ERROR: ', error.stack);
       res.json(error);
