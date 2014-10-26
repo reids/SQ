568e2003b71970e561360acad4f9fbfdbbf21a0e
Reid Sommerville <reid.sommerville@gmail.com>
2014-10-26 23:51:16 +0100

    This seems to have failed to commit, its the AMQ changes from a few days ago

--- server/lib/mongo-proxy.js
+++ server/lib/mongo-proxy.js
@@ -1,10 +1,13 @@
 var url = require('url');
 var qs = require('querystring');
 var https = require('https');
+var authservice = require('./authservice.js');
 
-module.exports = function(basePath, apiKey, dbName) {
+module.exports = function(mongoconf) {
 
-  basePath = url.parse(basePath);
+  var basePath = url.parse(mongoconf.dbUrl);
+  var apiKey = mongoconf.apiKey;
+  var dbName = mongoconf.dbName;
 
   // Map the request url to the mongolab url
   // @Returns a parsed Url object
@@ -37,32 +40,59 @@ module.exports = function(basePath, apiKey, dbName) {
 	  newReq.headers.host = newReq.hostname;
 	  return newReq;
   };
+  /**
+   * If we are writing a user then we intercept it so as to send the auth details to the 
+   * auth DB. We strip out the password and send a create request to the authservice
+   */
+  var doUserPreWriteTasks = function(req, res, next, done) {
+	  
+	  if (req.method !== 'POST' || req.params.collection !== 'users')
+		  done(req, res, next);
+	  else {
+		  // processing a user, we only have user writes, never updates
+		  var email = req.body.email;
+		  var password = req.body.password;
+		  delete req.body.password;
+		  
+		  authservice.create(email,password, function(user){
+			  if (user == null)
+					res.json(501, { statusText: 'failed to write authentication data'});
+			  else
+				  done(req, res, next)
+		  });
+	  }
+   }
 
+  /**
+   * Generic proxy for all db requests
+   */
   var proxy = function(req, res, next) {
     try {
       var options = mapRequest(req);
-      body=JSON.stringify(req.body);
-      // Create the request to the db
-      var dbReq = https.request(options, function(dbRes) {
-        var data = "";
-        res.headers = dbRes.headers;
-        dbRes.setEncoding('utf8');
-        dbRes.on('data', function(chunk) {
-          // Pass back any data from the response.
-          data = data + chunk;
-        });
-        dbRes.on('end', function() {
-          res.header('Content-Type', 'application/json');
-          res.statusCode = dbRes.statusCode;
-          res.httpVersion = dbRes.httpVersion;
-          res.trailers = dbRes.trailers;
-          res.send(data);
-          res.end();
-        });
+      doUserPreWriteTasks(req, res, next, function(req, res, next) {
+	      body=JSON.stringify(req.body);
+	      // Create the request to the db
+	      var dbReq = https.request(options, function(dbRes) {
+	        var data = "";
+	        res.headers = dbRes.headers;
+	        dbRes.setEncoding('utf8');
+	        dbRes.on('data', function(chunk) {
+	          // Pass back any data from the response.
+	          data = data + chunk;
+	        });
+	        dbRes.on('end', function() {
+	          res.header('Content-Type', 'application/json');
+	          res.statusCode = dbRes.statusCode;
+	          res.httpVersion = dbRes.httpVersion;
+	          res.trailers = dbRes.trailers;
+	          res.send(data);
+	          res.end();
+	        });
+	      });
+	      // Send any data the is passed from the original request, better way to detect an empty body?
+	      dbReq.write(JSON.stringify(req.body))
+	      dbReq.end();
       });
-      // Send any data the is passed from the original request, better way to detect an empty body?
-      dbReq.write(JSON.stringify(req.body))
-      dbReq.end();
     } catch (error) {
       console.log('ERROR: ', error.stack);
       res.json(error);
